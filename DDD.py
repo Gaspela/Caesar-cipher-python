f88047a5ec848f517356d253ba3806e76093e044
Samir millan <samirmillanorozco@hotmil.com>
2021-07-24 18:39:33 +0000

    Improve the code

--- DDD.py
+++ DDD.py
@@ -1,19 +1,25 @@
 # Caesar Cipher
 def inCode ():
-    text = input ("Enter your message:")
-    encryption = ''
+    text = str(input("Enter your message: "))
+    key = int(input("Encrypted level: "))
+    encryption = ""
     for char in text:
-        if not char.isalpha ():
-            continue
-        char = char.upper ()
-        code = ord (char) + 1
-        if code> ord ('Z'):
-            code = ord ('A')
-        encryption += chr (code)
 
+        if char.isupper():
+            charIndex = ord(char) - ord('A')
+            charShift = (charIndex + key) % 26 + ord('A')
+            charNew = chr(charShift)
+            encryption += charNew
+        elif char.islower():
+            charIndex = ord(char) - ord('a')
+            charShift = (charIndex + key) % 26 + ord('a')
+            charNew = chr(charShift)
+            encryption += charNew
+        else:
+            encryption += char
+            
     print (encryption)
 
-# Caesar Cipher - decrypt a message
 def desCode ():
     encryption = input ('Enter your cryptogram:')
     text = ''
@@ -27,6 +33,4 @@ def desCode ():
         text += chr (code)
     print (text)
 
-
-inCode ()
-desCode ()
+inCode()
